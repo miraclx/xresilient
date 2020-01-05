d84b4e90dcb15b6c4c8797da3716fd7f467cc448
Miraclx <omiraculous@gmail.com>
2020-01-05 13:54:48 +0100

    Added typings file

---
+++ typings/index.d.ts
@@ -0,0 +1,31 @@
+// Type definitions for xresilient
+// Project: https://github.com/miraclx/xresilient
+// Definitions by: Miraculous Owonubi <https://github.com/miraclx>
+
+/// <reference types="node" />
+
+import * as stream from 'stream';
+
+type ProtoExtends<T, U> = U & Omit<T, keyof U>;
+
+function xresilient<T>(
+  fn: (storeSlice: xresilient.ResilientStore<T>) => T, options?: xresilient.ResilientOpts
+): xresilient.ResilientStream<T>;
+
+interface _ResilientStream {
+  on(event: 'retry', listener: (retryCount: number) => void): this;
+}
+
+namespace xresilient {
+  interface ResilientOpts extends stream.ReadableOptions {
+    retries: number;
+  }
+  interface ResilientStore<T> {
+    retryCount: number;
+    bytesRead: number;
+    oldStream: T;
+  }
+  interface ResilientStream<T> extends ProtoExtends<NodeJS.ReadableStream, _ResilientStream> { }
+}
+
+export = xresilient;
